3cfa91dd901ca636c1e21bfa12506d66749d10f4
beidsjosh <joshua.beidham@griffithuni.edu.au>
2022-05-03 17:38:20 +1000

    added images to list of places on main screen

--- FavouritePlaces/PlaceRowView.swift
+++ FavouritePlaces/PlaceRowView.swift
@@ -11,7 +11,21 @@ struct PlaceRowView: View {
     @ObservedObject var placesrow: Place
     @Environment(\.managedObjectContext) var viewContext
     
+    var image: UIImage {
+        guard
+            let url = URL(string: placesrow.image!),
+            let data = try? Data(contentsOf: url)
+        else {
+            fatalError("Something went wrong!")
+        }
+        return UIImage(data: data)!
+    }
+    
     var body: some View {
+        Image(uiImage: image)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 30, height: 30)
         Text(placesrow.name ?? "")
     }
 }
