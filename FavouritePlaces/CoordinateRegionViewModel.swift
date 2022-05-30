d8151e0cce7ab470c726ac2bd5ed377bb58af6a2
beidsjosh <joshua.beidham@griffithuni.edu.au>
2022-05-30 15:25:05 +1000

    geocoding and sunrise/sunset now works on map, coords don't live update with user input however

--- FavouritePlaces/CoordinateRegionViewModel.swift
+++ FavouritePlaces/CoordinateRegionViewModel.swift
@@ -12,7 +12,7 @@ import MapKit
 
 class LocationViewModel: ObservableObject {
     @Published var location: CLLocation
-    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -27, longitude: -153), latitudinalMeters: 5000, longitudinalMeters: 5000)
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -27, longitude: -153), latitudinalMeters: 20000, longitudinalMeters: 20000)
     @Published var sunriseSunset = SunriseSunset(sunrise: "unknown", sunset: "unknown")
     @Published var name = ""
     var sunrise: String {
@@ -32,16 +32,20 @@ class LocationViewModel: ObservableObject {
         get { "\(location.coordinate.latitude)" }
         set {
             guard let newLatitude = Double(newValue) else { return }
-            let newLocation = CLLocation(latitude: newLatitude, longitude: location.coordinate.longitude)
-            location = newLocation
+            //let newLocation = CLLocation(latitude: newLatitude, longitude: location.coordinate.longitude)
+            let newLocation = CLLocationDegrees(newLatitude)
+            //location = newLocation
+            region.center.latitude = newLocation
         }
     }
     var longitudeString: String {
         get { "\(location.coordinate.longitude)" }
         set {
             guard let newLongitude = Double(newValue) else { return }
-            let newLocation = CLLocation(latitude: location.coordinate.latitude, longitude: newLongitude)
-            location = newLocation
+            //let newLocation = CLLocation(latitude: location.coordinate.latitude, longitude: newLongitude)
+            let newLocation = CLLocationDegrees(newLongitude)
+            //location = newLocation
+            region.center.longitude = newLocation
         }
     }
 
